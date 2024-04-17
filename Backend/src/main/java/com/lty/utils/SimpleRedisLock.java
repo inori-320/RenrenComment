1b028ef3ac2a67feea9da14fe6cef885efe70328
Li Tianyu <719471785@qq.com>
2024-04-17 21:13:55 +0800

    使用redis调用lua脚本实现释放锁的原子性，避免出现线程安全问题

--- Backend/src/main/java/com/lty/utils/SimpleRedisLock.java
+++ Backend/src/main/java/com/lty/utils/SimpleRedisLock.java
@@ -1,9 +1,14 @@
 package com.lty.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,6 +18,13 @@ public class SimpleRedisLock implements ILock{
     private String name;
     private StringRedisTemplate stringRedisTemplate;
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = "lock:" + name;
@@ -28,10 +40,14 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(name);
-        if(id.equals(threadId)){
-            stringRedisTemplate.delete(name);
-        }
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        String id = stringRedisTemplate.opsForValue().get(name);
+//        if(id.equals(threadId)){
+//            stringRedisTemplate.delete(name);
+//        }
+        // 调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
 }
