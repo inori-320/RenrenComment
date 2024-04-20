d19a53b14e33476d6bc97c87f488a189d57c83cf
Li Tianyu <719471785@qq.com>
2024-04-20 18:07:43 +0800

    基于set实现点赞功能

--- Backend/src/main/java/com/lty/service/IBlogService.java
+++ Backend/src/main/java/com/lty/service/IBlogService.java
@@ -15,4 +15,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IBlogService extends IService<Blog> {
 
     Result queryBlogById(Long id);
+
+    void likeBlog(Long id);
+
+    Result queryHotBlog(Integer current);
 }
--- Backend/src/main/java/com/lty/service/impl/BlogServiceImpl.java
+++ Backend/src/main/java/com/lty/service/impl/BlogServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lty.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lty.dto.Result;
 import com.lty.entity.Blog;
 import com.lty.entity.User;
@@ -7,8 +9,14 @@ import com.lty.mapper.BlogMapper;
 import com.lty.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lty.service.IUserService;
+import com.lty.utils.RedisConstants;
+import com.lty.utils.SystemConstants;
+import com.lty.utils.UserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -22,6 +30,21 @@ import org.springframework.stereotype.Service;
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
     @Autowired
     private IUserService userService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+    private void isBlogLiked(Blog blog) {
+        Long userId = UserHolder.getUser().getId();
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(RedisConstants.BLOG_LIKED_KEY, userId.toString());
+        blog.setIsLike(BooleanUtil.isTrue(isMember));
+    }
 
     @Override
     public Result queryBlogById(Long id) {
@@ -29,10 +52,48 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         if(blog == null){
             return Result.fail("笔记不存在！");
         }
-        Long userId = blog.getUserId();
-        User user = userService.getById(userId);
-        blog.setName(user.getNickName());
-        blog.setIcon(user.getIcon());
+        queryBlogUser(blog);
+        isBlogLiked(blog);
         return Result.ok(blog);
+    }
+
+    /**
+     * 用户点赞逻辑：每个用户只能点赞一次，重复点赞视为取消
+     * @param id
+     */
+    @Override
+    public void likeBlog(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        Boolean isMember = stringRedisTemplate.opsForSet().isMember(RedisConstants.BLOG_LIKED_KEY, userId.toString());
+        // 没赞过，可以点赞
+        if(BooleanUtil.isFalse(isMember)){
+            // 点赞数+1
+            boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
+            if(isSuccess){
+                stringRedisTemplate.opsForSet().add(RedisConstants.BLOG_LIKED_KEY, userId.toString());
+            }
+        } else {
+            // 否则取消点赞，点赞数-1
+            boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
+            if(isSuccess){
+                stringRedisTemplate.opsForSet().remove(RedisConstants.BLOG_LIKED_KEY, userId.toString());
+            }
+        }
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog -> {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);
+        });
+        return Result.ok(records);
     }
 }
