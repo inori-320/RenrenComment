957f93cf3f401ed03ca0d47746277b31614a585e
Li Tianyu <719471785@qq.com>
2024-04-22 16:32:05 +0800

    完成签到功能

--- Backend/src/main/java/com/lty/service/impl/UserServiceImpl.java
+++ Backend/src/main/java/com/lty/service/impl/UserServiceImpl.java
@@ -11,16 +11,22 @@ import com.lty.dto.UserDTO;
 import com.lty.entity.User;
 import com.lty.mapper.UserMapper;
 import com.lty.service.IUserService;
+import com.lty.utils.RedisConstants;
 import com.lty.utils.RegexUtils;
 import com.lty.utils.SystemConstants;
+import com.lty.utils.UserHolder;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import static com.lty.utils.RedisConstants.*;
@@ -85,6 +91,47 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 给token设置有效期，防止Redis被占满
         stringRedisTemplate.expire(LOGIN_USER_KEY + token, LOGIN_USER_TTL, TimeUnit.MINUTES);
         return Result.ok(token);
+    }
+
+    @Override
+    public Result sign() {
+        Long userId = UserHolder.getUser().getId();
+        LocalDateTime now = LocalDateTime.now();
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        // 获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        // 写入Redis
+        stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        Long userId = UserHolder.getUser().getId();
+        LocalDateTime now = LocalDateTime.now();
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        int dayOfMonth = now.getDayOfMonth();
+        // 获取本日截止到今天为止的签到记录
+        List<Long> result = stringRedisTemplate.opsForValue()
+                .bitField(key,
+                        BitFieldSubCommands.create()
+                                .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth))
+                                .valueAt(0));
+        if(result == null || result.isEmpty() || result.get(0) == null || result.get(0) == 0){
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        int ans = 0;
+        while(num > 0){
+            if((num & 1) == 0){
+                break;
+            }
+            ans ++;
+            num >>>= 1;
+        }
+        return Result.ok(ans);
     }
 
     private User createUserWithPhone(String phone) {
